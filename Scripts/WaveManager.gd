d0e747f4e8e8cb15afa4d2983a110248cecdd3d6
Ezioleq <git@ezioleq.com>
2022-03-19 23:16:35 +0100

    Add Amazing Wave System:tm:

---
+++ Scripts/WaveManager.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+export (PackedScene) var Enemy
+
+var current_wave = 0
+var timer = 0
+var wave_length = 0
+
+export var spawn_dist = 200
+export var spawn_bias = 100
+
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	rng.randomize()
+	pass
+
+func _process(delta):
+	timer += delta
+
+	if timer >= wave_length:
+		spawn_enemies(new_wave())
+		current_wave += 1
+		timer = 0
+
+	pass
+
+func new_wave():
+	var enemies = int(round(current_wave * 0.75) + round(rng.randf())) - int(
+		current_wave - rng.randf_range(0, current_wave * (current_wave % 3) * rng.randf())
+	)
+	var duration = int(enemies * 1.25) + rng.randi_range(0, 4)
+	
+	wave_length = duration
+	return enemies
+
+func spawn_enemy(pos: Vector2):
+	var e = Enemy.instance()
+	e.position = pos
+	get_tree().root.get_child(0).add_child(e)
+	pass
+
+func spawn_enemies(count):
+	for i in range(count):
+		var dist = spawn_dist + rng.randi_range(-spawn_bias, spawn_bias)
+		var dir = Vector2(
+			rng.randf_range(-1, 1),
+			rng.randf_range(-1, 1)
+		).normalized()
+		var pos = dir * dist
+
+		spawn_enemy(pos)
+	pass
